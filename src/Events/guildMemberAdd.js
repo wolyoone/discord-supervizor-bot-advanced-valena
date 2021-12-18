fb01c8e19900ca46df0a3e1757593dd9636f0afa
Wolyo <93871074+wolyoone@users.noreply.github.com>
2021-12-18 11:49:12 +0400

    Update guildMemberAdd.js

--- src/Events/guildMemberAdd.js
+++ src/Events/guildMemberAdd.js
@@ -15,9 +15,9 @@ module.exports = async (member) => {
   let rules = member.guild.channels.cache.find(x => x.id === conf.Rules)
 
   if (!durum){
-    member.setNickname(`${conf.Ktag} İsim ' Yaş`).catch()
+    member.setNickname(`${conf.Ktag} İsim | Yaş`).catch()
     member.roles.add([conf.Register.UnregRole]).catch()
-
+/*
     register.send(`
 Sunucumuza hoş geldin ${member}, Seninle beraber **${member.guild.memberCount}** kişiyiz! 
           
@@ -28,9 +28,9 @@ Sunucumuza kayıt olmak için tagımızı alıp ses teyit vermelisin.
 ${rules} kanalından kurallarımızı okumalısın.
 
 Sunucuya kayıt olduktan sonra kuralları okumuş kabul edeceğiz ve içeride yapılan cezai-işlemlerde bunu göz önünde bulunduracağız. İyi eğlenceler. :tada::tada::tada:`);
-      
+  */
 } else {
-  member.setNickname(`${conf.Ktag} Yeni ' Hesap`).catch()
+  member.setNickname(`${conf.Ktag} Yeni Hesap`).catch()
   member.roles.add([conf.FakeAccRole]).catch()
 
   return register.send(`
